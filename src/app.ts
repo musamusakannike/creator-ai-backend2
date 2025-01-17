ae70b084544e46bdd5ca5929ecc6d214cd3061ba
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-17 23:25:12 +0100

    Added passport and express session setup

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
-import express from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route';
-import connectDB from './config/db.config';
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import passport from "./config/passport.config";
+import session from "express-session";
+import authRoutes from "./routes/auth.route";
+import connectDB from "./config/db.config";
 
 dotenv.config();
 
 connectDB();
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/auth', authRoutes);
+// Session setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default-fallback-secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoutes);
 
 export default app;
