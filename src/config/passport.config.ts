48d2fc5784184ec32f6a0730a0b392308149c55f
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-18 14:38:32 +0100

    Added youtube access token to the user details

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -8,26 +8,22 @@ dotenv.config();
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || '',
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
-        // Find or create the user
         let user = await User.findOne({ googleId: profile.id });
-
         if (!user) {
           user = new User({
             googleId: profile.id,
             email: profile.emails?.[0]?.value,
-            youtubeAccessToken: accessToken, // Save the access token
+            youtubeAccessToken: accessToken,
           });
         } else {
-          // Update the user's access token if it exists
           user.youtubeAccessToken = accessToken;
         }
-
         await user.save();
         return done(null, user);
       } catch (error) {
