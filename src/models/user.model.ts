da1ac71f9376ef54afff51514b5658ae27bebd76
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-18 13:48:44 +0100

    Added youtube access token to user schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IUser extends Document {
   email: string;
   googleId: string;
+  youtubeAccessToken?: string; // Add the youtubeAccessToken property
 }
 
 const UserSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   googleId: { type: String, required: true },
+  youtubeAccessToken: {type: String, required: true}
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
