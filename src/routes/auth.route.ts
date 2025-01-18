e09d85a9c431e4e6e1d014036fd8278c4b4cbe64
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-18 14:39:22 +0100

    Increased api scope

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,14 +5,23 @@ import {
   loginFailure,
   logout,
 } from "../controllers/auth.controller";
-import { generateToken } from '../utils/jwt';
+import { generateToken } from "../utils/jwt";
 
 const router = Router();
 
 // Google Auth Route
 router.get(
   "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  passport.authenticate("google", {
+    scope: [
+      "profile",
+      "email",
+      "https://www.googleapis.com/auth/youtube.readonly",
+      "https://www.googleapis.com/auth/youtube.force-ssl",
+    ],
+    accessType: "offline", // Allows obtaining a refresh token
+    prompt: "consent", // Ensures user is prompted to grant permissions
+  })
 );
 
 // Google Auth Callback
@@ -21,7 +30,7 @@ router.get(
   passport.authenticate("google", { failureRedirect: "/auth/login" }),
   async (req, res): Promise<void> => {
     if (!req.user) {
-      res.status(401).json({ message: 'Authentication failed' });
+      res.status(401).json({ message: "Authentication failed" });
       return;
     }
     // Generate a JWT for the authenticated user
