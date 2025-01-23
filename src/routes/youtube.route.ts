376caad5aba27023ba1c5fe2d151329da67d017d
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-23 10:28:17 +0100

    Added route to get user contents

--- src/routes/youtube.route.ts
+++ src/routes/youtube.route.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth";
-import { getDashboardStats } from "../controllers/youtube.controller";
+import { getAllChannelContent, getDashboardStats } from "../controllers/youtube.controller";
 
 const router = Router();
+
+// Route for fetching dashboard analytics
 router.get(
   "/analytics",
   authenticate,
   getDashboardStats
+);
+
+// Route for fetching all channel content
+router.get(
+  "/contents",
+  authenticate,
+  getAllChannelContent
 );
 
 export default router;
